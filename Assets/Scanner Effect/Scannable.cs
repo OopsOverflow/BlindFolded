e4875dc71ed10283e04f30cb18100f18f52354fd
Nicolò Sonnino <swagpino@gmail.com>
2023-01-16 18:42:23 +0100

    feat: refactoring and aligning

---
+++ Assets/Scanner Effect/Scannable.cs
@@ -0,0 +1,15 @@
+namespace Scanner_Effect
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class Scannable : MonoBehaviour
+    {
+        public Animator UIAnim;
+
+        public void Ping()
+        {
+            UIAnim.SetTrigger("Ping");
+        }
+    }
+}

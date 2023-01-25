ce37742e04b0ec642f158c15e007265d73e1c925
Nicolò Sonnino <swagpino@gmail.com>
2023-01-25 16:02:52 +0100

    feat: first draft hand

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
+    public float speed;
+    
+    private Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AnimateHand();
+    }
+
+    public void SetGrip(float readValue)
+    {
+        gripTarget = readValue;
+    }
+
+    public void SetTrigger(float readValue)
+    {
+        triggerTarget = readValue;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent!=gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat("Grip", gripCurrent);
+        }
+        if (triggerCurrent!=triggerTarget) 
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat("Trigger", triggerCurrent);
+        }
         
     }
 }
---
+++ Build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.VRModule_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Single UnityEngine.XR.XRSettings::get_occlusionMaskScale()
+extern void XRSettings_get_occlusionMaskScale_mFA46FF99DD385D5D88247C356DA2B454B4C8132D (void);
+// 0x00000002 System.Void UnityEngine.XR.XRSettings::set_occlusionMaskScale(System.Single)
+extern void XRSettings_set_occlusionMaskScale_mFB321295E98DE1A01268FABCCB167CC2DCD8157E (void);
+// 0x00000003 UnityEngine.XR.XRSettings/StereoRenderingMode UnityEngine.XR.XRSettings::get_stereoRenderingMode()
+extern void XRSettings_get_stereoRenderingMode_mD66918C11E2216B1F8FA76934F79D5F85BC303FC (void);
+// 0x00000004 System.Void UnityEngine.XR.XRDevice::DisableAutoXRCameraTracking(UnityEngine.Camera,System.Boolean)
+extern void XRDevice_DisableAutoXRCameraTracking_m1243FCAD2AC9C4D5C2E551255A1B2BA266E12A52 (void);
+// 0x00000005 System.Void UnityEngine.XR.XRDevice::InvokeDeviceLoaded(System.String)
+extern void XRDevice_InvokeDeviceLoaded_mBE2198DE44A72E2F5059566C46B9907D82782790 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	XRSettings_get_occlusionMaskScale_mFA46FF99DD385D5D88247C356DA2B454B4C8132D,
+	XRSettings_set_occlusionMaskScale_mFB321295E98DE1A01268FABCCB167CC2DCD8157E,
+	XRSettings_get_stereoRenderingMode_mD66918C11E2216B1F8FA76934F79D5F85BC303FC,
+	XRDevice_DisableAutoXRCameraTracking_m1243FCAD2AC9C4D5C2E551255A1B2BA266E12A52,
+	XRDevice_InvokeDeviceLoaded_mBE2198DE44A72E2F5059566C46B9907D82782790,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	7372,
+	7198,
+	7338,
+	6607,
+	7192,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_VRModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_VRModule_CodeGenModule = 
+{
+	"UnityEngine.VRModule.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
